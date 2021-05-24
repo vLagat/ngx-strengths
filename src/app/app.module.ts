1412f533dc23885496106af575a7acf4f322910e
vLagat <lagatvick@gmail.com>
2021-05-24 14:34:48 +0300

    PT4A: Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { InitialComponent } from './views/initial.component';
+import { PatientListComponent } from './views/patient-list/patient-list.component';
+import { LocationsComponent } from './views/locations/locations.component';
+import { ReportsComponent } from './views/reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent,
+    PatientListComponent,
+    LocationsComponent,
+    ReportsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
