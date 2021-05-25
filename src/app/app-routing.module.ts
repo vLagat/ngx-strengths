1b157fef29c378af7173b85869c2a8be73e88036
vLagat <lagatvick@gmail.com>
2021-05-25 09:51:09 +0300

    PT4A: More updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+
+import { InitialComponent } from './views/initial.component';
+import { PatientListComponent } from './views/patient-list/patient-list.component';
+import { LocationsComponent } from './views/locations/locations.component';
+import { ReportsComponent } from './views/reports/reports.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/initial', pathMatch: 'full'},
+  { path: 'initial', component: InitialComponent },
+  { path: 'patient-list', component: PatientListComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'reports', component: ReportsComponent }
+];
 
 
 @NgModule({
