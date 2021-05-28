645a5bc9ac302add07eab45dc615fece6530013d
vLagat <lagatvick@gmail.com>
2021-05-28 09:24:57 +0300

    update

--- src/app/_model/patients.ts
+++ src/app/_model/patients.ts
@@ -5,13 +5,19 @@ export class Patients {
     gender: string;
     phone_number: string;
     date_created: string;
+    encounter_datetime: string;
+    location_id: string;
+    encounter_datetime_min: string;
 
-    constructor(patient_id, name,dob,gender,phone_number, date_created){
+    constructor(patient_id, name,dob,gender,phone_number, date_created,encounter_datetime,location_id,encounter_datetime_min){
         this.patient_id=patient_id;
         this.name=name;
         this.dob=dob;
         this.gender=gender;
         this.phone_number=phone_number;
         this.date_created=date_created;
+        this.encounter_datetime=encounter_datetime;
+        this.location_id=location_id;
+        this.encounter_datetime_min=encounter_datetime_min;
     }
 }
